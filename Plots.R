e193f158ea98b1d519ec74573e0743cddc664f36
liamkendall <lkendal2@myune.edu.au>
2018-02-10 22:10:37 +1100

    First plots, not the nicest

--- Plots.R
+++ Plots.R
@@ -18,25 +18,28 @@ require(gridExtra)
 #Hoverflies
 
 HOV1=ggplot(HOV,aes(x=log(Spec.wgt),y=log(IT)))+coord_equal(xlim=c(-7.5,-2.5),ylim=c(-0.5,2.5))+
-  geom_point(aes(colour=Country))+theme_bw()+theme(aspect.ratio=1)+ggtitle("Intertegular distance")
-+geom_smooth(method='lm',formula=log(HOV$Spec.wgt)~log(HOV$IT))
+  geom_point(aes(colour=Country),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("Hfly intertegular distance")
 
 HOV2=ggplot(HOV,aes(x=log(Spec.wgt),y=log(BL)))+coord_equal(xlim=c(-7.5,-2.5),ylim=c(1.75,3.25))+
-  geom_point(aes(colour=Country))+theme_bw()+theme(aspect.ratio=1)+ggtitle("Body length")
+  geom_point(aes(colour=Country),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("Hfly body length")
 
 HOV3=ggplot(HOV,aes(x=log(Spec.wgt),y=log(ITxBL)))+coord_equal(xlim=c(-7.5,-2.5),ylim=c(1.75,5.25))+
-  geom_point(aes(colour=Country))+theme_bw()+theme(aspect.ratio=1)+ggtitle("IT x BL")
+  geom_point(aes(colour=Country),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("Hfly IT x BL")
 
 grid.arrange(HOV1,HOV2,HOV3,ncol=3)
 
 #Bees
 BEE1=ggplot(BEE,aes(x=log(Spec.wgt),y=log(IT)))+coord_equal(xlim=c(-10,-0),ylim=c(-0.5,2.5))+
-  geom_point(aes(colour=Country))+theme_bw()+theme(aspect.ratio=1)+ggtitle("Intertegular distance")
+  geom_point(aes(colour=Country),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("Bee intertegular distance")
 BEE1
 BEE2=ggplot(BEE,aes(x=log(Spec.wgt),y=log(BL)))+coord_equal(xlim=c(-10,0),ylim=c(1,4))+
-  geom_point(aes(colour=Country))+theme_bw()+theme(aspect.ratio=1)+ggtitle("Body length")
+  geom_point(aes(colour=Country),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("Bee body length")
 BEE2
 BEE3=ggplot(BEE,aes(x=log(Spec.wgt),y=log(ITxBL)))+coord_equal(xlim=c(-10,0),ylim=c(0,7))+
-  geom_point(aes(colour=Country))+theme_bw()+theme(aspect.ratio=1)+ggtitle("IT x BL")
+  geom_point(aes(colour=Country),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("Bee IT x BL")
 BEE3
-grid.arrange(BEE1,BEE2,BEE3,ncol=3)
+
+Plots=grid.arrange(BEE1,BEE2,BEE3,HOV1,HOV2,HOV3,ncol=3,nrow=2)
+
+ggsave("Plots #1.pdf",Plots,device="pdf",width = 15, height = 7.5)
+

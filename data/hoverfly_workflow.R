cad8d24b408fc4e57a37ec31cc154b34e8d589f9
liamkendall <lkendal2@myune.edu.au>
2018-02-23 15:14:26 +0100

    Hoverfly workflow

---
+++ data/hoverfly_workflow.R
@@ -0,0 +1,66 @@
+##hoverfly_workflow
+
+#libraries
+require(MuMIn)
+require(broom)
+require(caret)
+
+data(hov_mean)
+data(hov_test)
+data(hov_train)
+
+##Diagnostic plots
+plot(Spec.wgt~IT,hov_mean)
+par(mfrow=c(2,2))
+plot(lm(Spec.wgt~IT,hov_mean))
+
+plot(lm(log(Spec.wgt)~log(IT),hov_mean))
+
+#1. #Full model - wo/ pres.time -Only one specimen - we can remove or keep
+options(na.action = "na.omit") 
+
+hov.full=lm(log(Spec.wgt) ~ log(IT) + Latitude + Sex + Subfamily + Country + #fix
+            log(IT):Latitude + log(IT):Sex + log(IT):Country + #interactions
+              log(IT):Subfamily,data=hov_mean)
+
+summary(hov.full)
+
+
+#DREDGE
+
+options(na.action = "na.fail")
+
+#Full model - w pres.time
+
+hov_dr=dredge(hov.full,beta="none",rank="AIC",trace=100) #think about "sd" and "AICc". AIC show same pattern.
+head(hov_dr)
+bee_dr[1:10]
+
+
+##WIth AICc  - Same again
+hov_dr_Cc=dredge(hov.full,beta="none",rank="AICc",trace=100) #think about "sd" and "AICc". AIC show same pattern.
+head(hov_dr_Cc)
+
+###Extract coefficients
+hov_dr_mods=get.models(hov_dr[1:5],subset=TRUE)
+
+
+#Retrieve the estimates (package broom)----
+
+hov_coef=lapply(hov_dr_mods[1:5],function (x) tidy(x))
+
+#Top five models
+summary(hov_dr_mods[1]$`142`) #Rsq 0.7492
+summary(hov_dr_mods[2]$`14`) #Rsq 0.7447
+summary(hov_dr_mods[3]$`143`) #Rsq 0.7407
+summary(hov_dr_mods[4]$`144`) #0.7474
+summary(hov_dr_mods[5]$`15`) #0.7344
+
+
+##File that will be updated - not sure about naming convention out of dredge
+hov_model=tidy(hov_dr_mods[1]$`142`)
+
+
+hov_mod_int=lm(formula = log(Spec.wgt) ~ Country + log(IT) + Sex + log(IT):Sex, data = hov_mean)
+summary(hov_mod_int)
+hov_mod=tidy(summary(hov_mod_int))

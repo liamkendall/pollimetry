350cc575c739b5301f3c6e16a955975cc4d14def
liamkendall <lkendal2@myune.edu.au>
2018-02-27 12:03:34 +0100

    27-2

---
+++ scripts/Preservative time workflow.R
@@ -0,0 +1,18 @@
+##Preservation time
+library(dplyr)
+
+#JUST AUST
+bee_country=split(bee_all,bee_all$Country)
+bee_aus=bee_country$Australia
+
+#Filter so only species with more than 1 preservative time
+
+bee_aus=as.data.frame(bee_aus %>%
+  group_by(Species) %>% 
+  filter(n_distinct(Pres.time)>1))
+
+pres.lme=lmer(log(Spec.wgt)~Pres.time+(1|Species),bee_aus)
+summary(pres.lme)
+
+##NOT SURE HOW TO TRANSFORM THIS BACK / APPLY CORRECTION
+

232284747c9b794e7beb81ff69c9be3a85c9c4ed
ibartomeus <ibartomeus@home>
2018-03-01 11:18:48 +0100

    Add ideas on foraging

--- scripts/foraging_workflow.R
+++ scripts/foraging_workflow.R
@@ -5,12 +5,26 @@ head(forage)
 str(forage)
 table(forage$Tribe)
 
+colnames(forage)
+pairs(forage[,c(15,16,19:21)])
+
+#Greenleaf 1 eq. per method of measure "type" 4 Types in greenleaf, but we have 12.
+#Plus different for mean, max,...
+# sample size greenleaf ~ 14 homing + 14 feeding + 70 other (in measurements, which is ~ 50 species)
+# We do have ~ 107 species (x2!)
+# test for sociality in a PGLS Forage ~ IT * Soc (PGLS)
+# test rmse
+# + 3 + 3 + 2 hoverflies ~ 7 ... 
+# We provide an itertive framewwork!
+
+
+
 options(stringsAsFactors = TRUE)
 
 forage[1:152,c("Spec.wgt")]=exp(predict(Flm,newdata=forage[1:152,]))
 
-match with this which function
-bee_mean[which(bee_mean$Species=='Homalictus_urbanus' ),]
+#match with this which function
+#bee_mean[which(bee_mean$Species=='Homalictus_urbanus' ),]
 
 
 forage[135:263,c("Spec.wgt")]=exp(predict(Genus.lm,newdata=forage[135:265,]))

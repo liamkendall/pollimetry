e67d23ffd0672e6d12e3e13075c72609195a19e6
liamkendall <lkendal2@myune.edu.au>
2018-05-04 09:35:00 +1000

    4-5-18

--- scripts/Plots.R
+++ scripts/Plots.R
@@ -5,16 +5,14 @@ require(ggplot2)
 require(gridExtra)
 
 #Bees
-BEE1=ggplot(bee_all,aes(x=log(Spec.wgt),y=log(IT),colour=Region))+coord_equal(xlim=c(-2,6),ylim=c(-1,2))+
+BEE1=ggplot(bee_all,aes(x=(Spec.wgt),y=(IT),colour=Region))+coord_equal(xlim=c(0,320),ylim=c(0,8))+
 geom_point(aes(),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("ITD")
 BEE1
-BEE2=ggplot(bee_all,aes(x=log(Spec.wgt),y=log(IT),colour=Family))+coord_equal(xlim=c(-2,6),ylim=c(-1,2))+
-  geom_smooth(method="lm", formula=y~x,se=F)+
+BEE2=ggplot(bee_all,aes(x=(Spec.wgt),y=(IT),colour=Family))+coord_equal(xlim=c(0,320),ylim=c(0,8))+
   geom_point(aes(),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("ITD")
 BEE2
-BEE3=ggplot(bee_all,aes(x=log(Spec.wgt),y=log(IT),colour=Sex))+coord_equal(xlim=c(-2,6),ylim=c(-1,2))+
-  geom_smooth(method="lm", formula=y~x,se=F)+
-  geom_point(aes(),lwd=2,shape=3)+theme_bw()+theme(aspect.ratio=1)+ggtitle("ITD")
+BEE3=ggplot(bee_all,aes(x=(Spec.wgt),y=(IT),colour=Family))+coord_equal(xlim=c(0,50),ylim=c(0.5,5))+
+  geom_point(aes(),lwd=2,shape=1)+theme_bw()+theme(aspect.ratio=1)+ggtitle("ITD")
 BEE3
 
 Plots=grid.arrange(BEE1,BEE2,BEE3,ncol=3,nrow=1)
